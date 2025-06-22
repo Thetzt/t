aefb757b1f790382d92546a15bb90516b50bcdf9
Thetzt <122680531+Thetzt@users.noreply.github.com>
2025-06-23 05:09:28 +0630

    Update check-api.js

--- api/check-api.js
+++ api/check-api.js
@@ -1,11 +1,13 @@
 export default async function handler(req, res) {
+  const apiKey = process.env.JAP_API_KEY;
+
   const response = await fetch("https://justanotherpanel.com/api/v2", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      key: "7590d2173f1e2fe1ef245261a99aa5bf",
+      key: apiKey,
       action: "balance"
     })
   });
